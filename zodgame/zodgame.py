ecc678bbb03c5df9984c61b4f6ff19dec1273f6e
ty <653297351@qq.com>
2022-02-28 00:49:26 +0800

    Add auto task module. (#12)
    * Update README.md
    
    * Update zodgame.py

--- zodgame/zodgame.py
+++ zodgame/zodgame.py
@@ -7,8 +7,10 @@ import subprocess
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
 
 import undetected_chromedriver as uc
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 
-def getDriverVersion():
+def get_driver_version():
    cmd = r'''powershell -command "&{(Get-Item 'C:\Program Files\Google\Chrome\Application\chrome.exe').VersionInfo.ProductVersion}"'''
    try:
        out, err = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
@@ -18,14 +20,82 @@ def getDriverVersion():
        print('Check chrome version failed:{}'.format(e))
        return 0
 
+def zodgame_checkin(driver, formhash):
+    checkin_url = "https://zodgame.xyz/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&inajax=0"    
+    checkin_query = """
+        (function (){
+        var request = new XMLHttpRequest();
+        var fd = new FormData();
+        fd.append("formhash","%s");
+        fd.append("qdxq","kx");
+        request.open("POST","%s",false);
+        request.withCredentials=true;
+        request.send(fd);
+        return request;
+        })();
+        """ % (formhash, checkin_url)
+    checkin_query = checkin_query.replace("\n", "")
+    driver.set_script_timeout(240)
+    resp = driver.execute_script("return " + checkin_query)
+    match = re.search('<div class="c">\n(.*?)</div>\n', resp["response"], re.S)
+    message = match[1] if match is not None else "签到失败"
+    print(f"【签到】{message}")
+    return "恭喜你签到成功!" in message or "您今日已经签到，请明天再来" in message
+
+
+def zodgame_task(driver, formhash):
+    driver.get("https://zodgame.xyz/plugin.php?id=jnbux")
+    WebDriverWait(driver, 240, 5).until(
+        lambda x: x.title != "Just a moment..."
+    )
+
+    join_bux = driver.find_elements(By.XPATH, '//font[text()="开始参与任务"]')
+    if len(join_bux) != 0 :    
+        driver.get(f"https://zodgame.xyz/plugin.php?id=jnbux:jnbux&do=join&formhash={formhash}")
+        WebDriverWait(driver, 240, 5).until(
+            lambda x: x.title != "Just a moment..."
+        )
+        zodgame_task(driver, formhash)
+
+    join_task_a = driver.find_elements(By.XPATH, '//a[text()="参与任务"]')
+    success = True
+
+    if len(join_task_a) == 0:
+        print("【任务】所有任务均已完成。")
+        return success
+
+    for idx, a in enumerate(join_task_a):
+        on_click = a.get_attribute("onclick")
+        try:
+            function = re.search("openNewWindow(.*)\(\);", on_click, re.S)[0]
+            resp = driver.execute_script(function)
+            driver.switch_to.window(driver.window_handles[-1])
+            WebDriverWait(driver, 240, 5).until(
+                lambda x: x.find_elements(By.XPATH, '//div[text()="成功！"]')
+            )
+            check_url = re.search('plugin.php(.*)page=0', on_click, re.S)[0]
+            driver.get(f"https://zodgame.xyz/{check_url}")
+            WebDriverWait(driver, 240, 5).until(
+                lambda x: x.find_elements(By.XPATH, '//p[text()="检查成功, 积分已经加入您的帐户中"]')
+            )
+            driver.close()
+            driver.switch_to.window(driver.window_handles[0])
+            print(f"【任务】任务 {idx+1} 成功。")
+        except:
+            print(f"【任务】任务 {idx+1} 失败。")
+            success = False
+            continue
+    return success
+
 def zodgame(cookie_string):
     options = uc.ChromeOptions()
     options.add_argument("--disable-popup-blocking")
     #options.add_argument("--no-sandbox")
-    
-    version = getDriverVersion()
+      
+    version = get_driver_version()
     driver = uc.Chrome(version_main=version, options = options)
 
+    # Load cookie
     driver.get("https://zodgame.xyz/")
 
     cookie_string = cookie_string.replace("/","%2")
@@ -43,46 +113,24 @@ def zodgame(cookie_string):
                 "value": cookie["value"],
                 "path": "/",
             })
-
-    driver.get("https://zodgame.xyz/")
-    time.sleep(5)
-
-    #driver.execute_script('window.location.href="https://zodgame.xyz/";')
-    timesleep=0
-    while driver.title == "Just a moment...":
-        time.sleep(5)
-        timesleep = timesleep + 5
-        assert timesleep <= 240, "签到超时"
-
-    formhash = driver.find_element(uc.selenium.webdriver.common.by.By.XPATH, '//input[@name="formhash"]').get_attribute('value')
-    checkin_url = "https://zodgame.xyz/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&inajax=0"    
-    checkin_query = """
-        (function (){
-        var request = new XMLHttpRequest();
-        var fd = new FormData();
-        fd.append("formhash","%s");
-        fd.append("qdxq","kx");
-        request.open("POST","%s",false);
-        request.withCredentials=true;
-        request.send(fd);
-        return request;
-        })();
-        """ % (formhash, checkin_url)
-    checkin_query = checkin_query.replace("\n", "")
-    resp = driver.execute_script("return " + checkin_query)
-    match = re.search('<div class="c">\n(.*?)</div>\n',resp["response"],re.S)
-    message = match.group(1) if match is not None else "签到失败"
-    print(message)
-    assert "恭喜你签到成功!" in message or "您今日已经签到，请明天再来" in message
     
+    driver.get("https://zodgame.xyz/")
+    
+    try:
+        WebDriverWait(driver, 240, 5).until(
+            lambda x: x.title != "Just a moment..."
+        )
+        formhash = driver.find_element(By.XPATH, '//input[@name="formhash"]').get_attribute('value')
+    except:
+        assert False, "Login fails. Please check your cookie."
+    
+    assert zodgame_checkin(driver, formhash) and zodgame_task(driver, formhash), "Checkin failed or task failed."
+
     driver.close()
     driver.quit()
     
 if __name__ == "__main__":
     cookie_string = sys.argv[1]
-    getDriverVersion()
-    if cookie_string:
-        zodgame(cookie_string)
-    else:
-        print("未配置Cookie")
-   
+    assert cookie_string
+    
+    zodgame(cookie_string)
